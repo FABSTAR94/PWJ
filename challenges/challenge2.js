1dde187da9c03b8a5cb8dcd753607a239028a184
Faviola Caballero <fabycg18@gmail.com>
2020-09-30 14:42:20 -0700

    converted minutes to seconds

---
+++ challenges/challenge2.js
@@ -0,0 +1,8 @@
+//CHALLENGE 2: Convert minutes to seconds.
+// 1 minute = 60 seconds.
+
+function convertMinutesToSeconds(minutes) {
+  return Math.floor(minutes * 60);
+}
+
+console.log(convertMinutesToSeconds(5));
