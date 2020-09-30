9ab49a2c6d48f93200bb8ef674c1b3265e75a8a6
Faviola Caballero <fabycg18@gmail.com>
2020-09-30 15:07:15 -0700

    age in seconds

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -1,9 +1,9 @@
 //CHALLENGE 3: Your age in seconds.
-//12 months in a year
-//30 days in a month
-//24 hours in a day
-//1 hours there is 60 minutes
-//1 minute there is 60 seconds
+//12 months in a year.
+//30 days in a month.
+//24 hours in a day.
+//1 hours there is 60 minutes.
+//1 minute there is 60 seconds.
 
 function yourAgeInSeconds(age) {
   return "Your age is " + age * 12 * 30 * 24 * 60 * 60 + " in seconds";
